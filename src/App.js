294aa02290c774b6e0f1ff2e917d7cb84efce405
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-02-06 20:53:12 -0300

    Fazendo rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Header from './Header'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+
+const Generos = () => {
+  return <h1>Gêneros</h1>
+}
 
 function App() {
   return (
-    <div>
-      <Header/>
-    </div>
+    <Router>
+      <div>
+        <Header/>
+        <Route path="/" exact component={Home} />
+        <Route path="/generos" component={Generos} />
+      </div>
+    </Router>
   )
 }
 
--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,7 @@ import {
   NavLink,
   NavbarToggler
 } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   const [open, setOpen] = useState(false)
@@ -18,12 +19,12 @@ const Header = () => {
   return (
     <div>
       <Navbar color='light' light expand='md'>
-        <NavbarBrand>Minhas Séries</NavbarBrand>
+        <NavbarBrand tag={Link} to='/'>Minhas Séries</NavbarBrand>
         <NavbarToggler onClick={toggle}/>
         <Collapse isOpen={open} navbar> 
           <Nav className='ml-auto' navbar>
             <NavItem>
-              <NavLink href='/'>Genêros</NavLink>
+              <NavLink tag={Link} to='/generos'>Genêros</NavLink>
             </NavItem>
           </Nav>
         </Collapse>
